8e1147fa94fc202868706da3d049f5ac88903376
Dmitriy Marianchenko <dima.marjan@gmail.com>
2021-04-05 20:30:08 +0300

    6 done

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,9 @@
 const inputField = document.querySelector("#validation-input");
 
-inputField.addEventListener("blur", () =>
-  
-);
+inputField.addEventListener("blur", () => {
+  if (inputField.value.length < parseInt(inputField.dataset.length)) {
+    inputField.classList.add("invalid");
+  } else {
+    inputField.classList = "valid";
+  }
+});
